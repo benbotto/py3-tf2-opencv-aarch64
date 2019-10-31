2e1677b1cc801dd9db9e50af14fcd49d69897f12
Ben Botto <benjamin.botto@gmail.com>
2019-10-31 13:03:23 -0700

    Version 1.0.0.

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+TAG=$1
+
 docker build \
   -t avejidah/py3-tf2-opencv4:latest \
   .
+
+docker push avejidah/py3-tf2-opencv4:latest
+docker tag avejidah/py3-tf2-opencv4:latest avejidah/py3-tf2-opencv4:${TAG}
+docker push avejidah/py3-tf2-opencv4:${TAG}
